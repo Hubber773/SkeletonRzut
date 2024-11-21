985c574f792be82268f145e54d7a0e42a0e3395a
Hubber773 <github53@gmail.com>
2024-11-21 11:10:15 +0100

    Zdięć nie ma ale text wystarczy chyba, wygląda spoko

--- WpfApp6/MainWindow.xaml.cs
+++ WpfApp6/MainWindow.xaml.cs
@@ -9,16 +9,71 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfApp6
+namespace SkeletonRzut
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private int liczbaKostek = 1;
+        private int liczbaScian = 4;
+        private int wynikOgolnyWartosc = 0;
+        private static Random rand = new Random();
+
         public MainWindow()
         {
             InitializeComponent();
+            Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            liczbaKostekValue.Text = liczbaKostek.ToString();
+            liczbaScianValue.Text = liczbaScian.ToString();
+        }
+
+        private void LiczbaKostekSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (liczbaKostekValue != null)
+            {
+                liczbaKostek = (int)e.NewValue;
+                liczbaKostekValue.Text = liczbaKostek.ToString();
+            }
+        }
+
+        private void LiczbaScianSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (liczbaScianValue != null)
+            {
+                liczbaScian = (int)e.NewValue;
+                liczbaScianValue.Text = liczbaScian.ToString();
+            }
+        }
+
+        private void RzucKostkami_Click(object sender, RoutedEventArgs e)
+        {
+            wynikiPanel.Children.Clear();
+            int wynikAktualnyRzut = 0;
+
+            for (int i = 0; i < liczbaKostek; i++)
+            {
+                int rzut = rand.Next(1, liczbaScian + 1);
+                wynikAktualnyRzut += rzut;
+                TextBlock wynikTextBlock = new TextBlock
+                {
+                    Text = rzut.ToString(),
+                    Margin = new Thickness(5)
+                };
+                wynikiPanel.Children.Add(wynikTextBlock);
+            }
+
+            wynikOgolnyWartosc += wynikAktualnyRzut;
+            wynikAktualny.Text = $"Wynik aktualnego rzutu: {wynikAktualnyRzut}";
+            wynikOgolny.Text = $"Wynik ogólny: {wynikOgolnyWartosc}";
+        }
+
+        private void ResetujWynik_Click(object sender, RoutedEventArgs e)
+        {
+            wynikOgolnyWartosc = 0;
+            wynikOgolny.Text = "Wynik ogólny: 0";
         }
     }
 }
